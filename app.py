9b4b15f3fa35d12e4466e1d12123bb10fbb36025
lucienhoang <nosewater.station@gmail.com>
2026-09-05 21:42:46 +0700

    Connect GitHub API data to Top 10 repos tab with Plotly chart

--- app.py
+++ app.py
@@ -1,16 +1,44 @@
 import streamlit as st
+import requests
+import plotly.express as px
+
+
+def get_python_repos():
+    """Get repositories from GitHub API."""
+    url = "https://api.github.com/search/repositories?q=language:python&sort=stars"
+    r = requests.get(url)
+    return r
+
 
 st.title("GitHub Explorer")
 
-tab1, tab2 = st.tabs(["🔍 Find a repo", "🏆 Top 10 Python Repos"])
+tab1, tab2 = st.tabs(["🔍 Repo Lookup", "🏆 Top 10 Python Repos"])
 
 with tab1:
-    st.header("Search for a Repository")
-    repo_name = st.text_input("Enter repository name (e.g., pytorch/pytorch)")
+    st.header("Repository Search")
+    repo_name = st.text_input("Enter a repo name (e.g., pytorch/pytorch)")
     if repo_name:
         st.write(f"You entered: {repo_name}")
 
 with tab2:
     st.header("Top 10 Python Repositories by Stars")
     if st.button("Fetch Data"):
-        st.write("The API will be called here...")
+        r = get_python_repos()
+        # Convert the JSON response into a Python dict.
+        response_dict = r.json()
+        # Store the list of repo dictionaries.
+        repo_dicts = response_dict["items"][:10]
+
+        # Prepare information for plot.
+        names = [repo["name"] for repo in repo_dicts]
+        stars = [repo["stargazers_count"] for repo in repo_dicts]
+
+        fig = px.bar(
+            x=names,
+            y=stars,
+            labels={"x": "Repository", "y": "Stars"},
+            title="Most-Starred Python Projects on GitHub",
+        )
+
+        # Embeded the plot into page.
+        st.plotly_chart(fig)
